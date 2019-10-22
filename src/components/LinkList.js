42e481f5667a912fd211413ff9a543eb32ec3df3
Sebastian Belmar <si.belmar@gmail.com>
2019-10-21 23:11:01 -0700

    Init fetch

---
+++ src/components/LinkList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import Link from './Link'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const FEED_QUERY = gql`
+  {
+    links {
+      id
+      url
+      description
+    }
+  }
+`
+
+class LinkList extends Component {
+  render() {
+    return (
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>
+          if (error) return <div>Error</div>
+
+          const linkToRender = data.links
+
+          console.log(data)
+
+          return (
+            <div>
+              {linkToRender.map(link => <Link key={link.id} link={link} />)}
+            </div>
+          )
+        }}
+      </Query>
+    )
+  }
+}
+
+export default LinkList
