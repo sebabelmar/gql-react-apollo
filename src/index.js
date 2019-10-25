946cb8f6d6e9123f1599ed665452047de57e29ad
Sebastian Belmar <si.belmar@gmail.com>
2019-10-24 19:23:14 -0700

    Add middle ware for authentication

--- src/index.js
+++ src/index.js
@@ -10,6 +10,19 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BrowserRouter } from 'react-router-dom'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 
 // 2
 const httpLink = createHttpLink({
@@ -18,7 +31,7 @@ const httpLink = createHttpLink({
 
 // 3
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
